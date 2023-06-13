49c97a1c68b7e5618c1c553270bae28647f2edcc
Software Shinobi <troy@softwareshinobi.online>
2023-06-13 13:59:22 -0500

    adding more heat

--- source-code/replace-content-between-two-strings.sh
+++ source-code/replace-content-between-two-strings.sh
@@ -3,16 +3,16 @@ reset
 
 clear
 
-## find live-content/ -iname '*.html' -execdir python3 {} replace-content-between-two-strings.py  \;
+## find  -iname '*.html' -execdir python3 {} replace-content-between-two-strings.py  \;
 
 ## 
 
-python3 replace-content-between-two-strings.py replace-content-between-two-strings.py live-content/index.html
+python3 replace-content-between-two-strings.py replace-content-between-two-strings.py index.html
 
-python3 replace-content-between-two-strings.py replace-content-between-two-strings.py live-content/tool-clean-and-format-markdown.html
+python3 replace-content-between-two-strings.py replace-content-between-two-strings.py tool-clean-and-format-markdown.html
 
-python3 replace-content-between-two-strings.py replace-content-between-two-strings.py live-content/tool-convert-markdown-proprietary-format.html
+python3 replace-content-between-two-strings.py replace-content-between-two-strings.py tool-convert-markdown-proprietary-format.html
 
-python3 replace-content-between-two-strings.py replace-content-between-two-strings.py live-content/tool-system-monitoring.html
+python3 replace-content-between-two-strings.py replace-content-between-two-strings.py tool-system-monitoring.html
 
-python3 replace-content-between-two-strings.py replace-content-between-two-strings.py live-content/tool-calculate-text-difference-percentage.html
+python3 replace-content-between-two-strings.py replace-content-between-two-strings.py tool-calculate-text-difference-percentage.html
